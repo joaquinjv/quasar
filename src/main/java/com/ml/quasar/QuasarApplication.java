8baccad442eb9961fdd401463448bc3ccc76b07c
joaquinjv <joaquin547@gmail.com>
2022-02-14 20:35:28 -0300

    code commit

--- src/main/java/com/ml/quasar/QuasarApplication.java
+++ src/main/java/com/ml/quasar/QuasarApplication.java
@@ -3,15 +3,20 @@ package com.ml.quasar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@RestController
 public class QuasarApplication {
-	
+
 	public static void main(String[] args) {
-		
 		SpringApplication.run(QuasarApplication.class, args);
-		
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world!";
 	}
 
 }
- 
