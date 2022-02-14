ff0b2fdfaa71e47aed1f329d11808e7b27f7acca
joaquinjv <joaquin547@gmail.com>
2022-02-14 00:14:36 -0300

    code commit

---
+++ WEB-INF/general-error.jsp
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<!-- Basic MetaFields -->
+	    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <title>Quasar</title>
+	</head>
+	<body>
+		<p>Hubo un error inesperado. (404)</p>
+	</body>
+</html>
--- src/main/java/com/ml/quasar/QuasarApplication.java
+++ src/main/java/com/ml/quasar/QuasarApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }, scanBasePackages="quasar")
 public class QuasarApplication {
 	
 	public static void main(String[] args) {
@@ -14,3 +14,4 @@ public class QuasarApplication {
 	}
 
 }
+ 
