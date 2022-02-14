af1ad39ed91349f39404f795a5871fa63e6cac8f
joaquinjv <joaquin547@gmail.com>
2022-02-14 00:37:48 -0300

    code commit

--- src/main/java/com/ml/quasar/QuasarApplication.java
+++ src/main/java/com/ml/quasar/QuasarApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }, scanBasePackages="quasar")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class QuasarApplication {
 	
 	public static void main(String[] args) {
